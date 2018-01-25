969968793c0b7324456ca82b94c2ada4e94fd903
iainb <iainspeed@gmail.com>
2018-01-25 17:20:34 +0900

    Better debugging outpup, docs, and structured to allow the local paths coming up.

--- path_helper.rb
+++ path_helper.rb
@@ -9,34 +9,68 @@ ETC_PATHS_D       = Pathname("/etc/paths.d")  # a directory
 
 CURRENT_PATH = ARGV.shift || ENV["PATH"]
 
-def read_file file
-  File.readlines( file ).map(&:chomp)
+def output_debug_lines lines
+	warn lines.map{|x| "\t#{x}\n" }.join if ENV["DEBUG"]
 end
 
-def path_helper
+def read_file file
+  lines = File.readlines( file ).map(&:chomp)
+  output_debug_lines lines
+  lines
+end
+
+
+# Expects a layout of:
+# 	DIR/paths
+# 	DIR/paths.d/*
+def path_helper paths_file, paths_dir
   # Simplest way, slurp the file contents into an array
   @entries = []
 
-  Pathname(ETC_PATHS_D).each_child do |file|
-    next unless file.file?
-    next if file.basename.to_s =~ /^\./
-    @entries += read_file file
-  end
-
-  @entries += read_file Pathname(ETC_PATHS)
-
-	unless CURRENT_PATH.nil? or CURRENT_PATH.empty?
-		@entries += CURRENT_PATH.split(/\:+/)
+	pathsd = Pathname(paths_dir)
+	if pathsd.exist?
+		warn "Getting paths from #{pathsd}" if ENV["DEBUG"]
+		pathsd.each_child do |file|
+			next unless file.file?
+			next if file.basename.to_s =~ /^\./
+			@entries += read_file file
+		end
 	end
-  @entries.uniq.join(":")
+
+	paths = Pathname(paths_file)
+	if paths.exist?
+		warn "Getting paths from #{paths}"
+		@entries += read_file paths
+	end
+  @entries
 end
 
+def join_up entries
+	entries.reject(&:empty?).uniq.join(":")
+end
+
+warn "DEBUG MODE" if ENV["DEBUG"]
+
+entries = []
+[
+	[ETC_PATHS, ETC_PATHS_D],
+].each do |(paths_file, paths_dir)|
+	entries += path_helper( paths_file, paths_dir )
+end
+
+unless CURRENT_PATH.nil? or CURRENT_PATH.empty?
+	warn "Current path:"
+	lines = CURRENT_PATH.split(/\:+/)
+  output_debug_lines lines
+	entries += lines
+end
+
+final = join_up entries
+
 if ENV["DEBUG"]
-  warn "DEBUG MODE"
-  warn path_helper
+  warn "PATH:\n#{final}"
 else
- path = path_helper
- print path
+ print final
 end
 
 exit 0
